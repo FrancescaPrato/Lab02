59d5fca1b05abbf72ded3bd750b4e51c1ca6b48c
francesca <francesca@172.20.39.38>
2018-03-14 10:58:04 +0100

    Lab 02 finito a lezione

--- Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
+++ Lab2_Alien/src/it/polito/tdp/alien/AlienController.java
@@ -5,17 +5,21 @@ package it.polito.tdp.alien;
  */
 
 
-
-import java.net.URL;
+import java.math.*;
+import java.net.*;
+import java.util.LinkedList;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class AlienController {
+	
+	LinkedList <paroleAliene> dizionario;
+	String testo;
 	
     @FXML
     private ResourceBundle resources;
@@ -38,18 +42,54 @@ public class AlienController {
     	assert btnTranslate != null : "fx:id=\"bntTranslate\" was not injected: check your FXML file 'Alien.fxml'.";
     	assert btnReset != null : "fx:id=\"btnReset\" was not injected: check your FXML file 'Alien.fxml'.";
     	
+    	testo = new String();
+    	dizionario= new LinkedList<paroleAliene>();
     }
   
     
     @FXML
     void doTranslate(ActionEvent event) {
+    	
+    	testo = txtWord.getText().toLowerCase();
+    	String array[] =testo.split(" ");
+    	if(testo.contains("0")||testo.contains("1")||testo.contains("2")||testo.contains("3")||testo.contains("4")||testo.contains("5")||testo.contains("6")||testo.contains("7")||testo.contains("8")||testo.contains("9"))
+    	{
+    		txtResult.setText("Stringa alfanumerica non valida!!");
+    	}
+    	else {
+		    	if(testo.contains(" ")) {
+		    		paroleAliene parola = new paroleAliene(array[0],array[1]);
+		    		for(paroleAliene p : dizionario) {
+		    			if(p.getParola().compareTo(parola.getParola())==0) 
+		    				if(p.getTraduzione().compareTo(array[1])!=0)
+		    					p.setTraduzione(p.getTraduzione()+"-"+array[1]);
+		    			}
+		    		
+		    		dizionario.add(parola);
+		    		txtWord.clear();
+		    		txtResult.setText("La traduzione di : "+parola.getParola()+" è "+parola.getTraduzione()+"\n");
+		    	}
+		    	else
+		    	{
+		    		
+		    		for(paroleAliene p : dizionario) {
+		    			if(p.getParola().compareTo(testo)==0) {
+		    				System.out.println("ciao");
+		    				
+		    				txtResult.setText("La traduzione di : "+p.getParola()+" è "+p.getTraduzione()+"\n");
+		    				return;
+		    			}
+		    		}
+		    		txtResult.setText("La traduzione di : "+testo+" non è presente ");
+		    	}
+    	}
     	    	
     }
     
     
     @FXML
     void doReset(ActionEvent event) {
-
+    	txtResult.clear();
     }
     
 }
